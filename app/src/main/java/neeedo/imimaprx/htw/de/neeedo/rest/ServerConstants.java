761e2bab10bd4ce5f8d92e9fc0047b3b465d775b
stephan lindauer <stephan.lindauer@gmail.com>
2015-02-04 10:36:04 +0100

    removed comment

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/ServerConstants.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/ServerConstants.java
@@ -1,8 +1,5 @@
 package neeedo.imimaprx.htw.de.neeedo.rest;
 
-/**
- * Created by Admin on 31.01.2015.
- */
 public abstract class ServerConstants {
     public static final String LOCALHOST_ADDRESS_STUB = "http://10.0.2.2:9000/stub/";
     public static final String LOCALHOST_ADDRESS = "http://10.0.2.2:9000/";
