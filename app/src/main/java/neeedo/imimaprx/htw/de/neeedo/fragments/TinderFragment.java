2ffe755d27cf4731c7901d9727a7ca3779da1955
stephan lindauer <stephan.lindauer@gmail.com>
2015-02-04 15:44:30 +0100

    complete reset for swipe view to start from scratch

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/TinderFragment.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/TinderFragment.java
@@ -1,125 +1,21 @@
 package neeedo.imimaprx.htw.de.neeedo.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.lorentzos.flingswipe.SwipeFlingAdapterView;
-
-import java.util.ArrayList;
-
-import neeedo.imimaprx.htw.de.neeedo.R;
 
 public class TinderFragment extends Fragment {
-
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
-
-    private int i;
-
-    private SwipeFlingAdapterView flingContainer;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-
-        View view = inflater.inflate(R.layout.swiper, container, false);
-
-        return view;
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        flingContainer = (SwipeFlingAdapterView) getActivity().findViewById(R.id.frame);
-
-//        getActivity().setContentView(R.layout.swiper);
-
-
-        al = new ArrayList<>();
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");
-        al.add("html");
-        al.add("c++");
-        al.add("css");
-        al.add("javascript");
-
-        arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.item, R.id.helloText, al);
-//        flingContainer.setAdapter(arrayAdapter);
-//        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
-//            @Override
-//            public void removeFirstObjectInAdapter() {
-//                Log.d("LIST", "removed object!");
-//                al.remove(0);
-//                arrayAdapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onLeftCardExit(Object dataObject) {
-//                makeToast(getActivity(), "Left!");
-//            }
-//
-//            @Override
-//            public void onRightCardExit(Object dataObject) {
-//                makeToast(getActivity(), "Right!");
-//            }
-//
-//            @Override
-//            public void onAdapterAboutToEmpty(int itemsInAdapter) {
-//                al.add("XML ".concat(String.valueOf(i)));
-//                arrayAdapter.notifyDataSetChanged();
-//                Log.d("LIST", "notified");
-//                i++;
-//            }
-//
-//            @Override
-//            public void onScroll(float scrollProgressPercent) {
-//                View view = flingContainer.getSelectedView();
-//                view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
-//                view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
-//            }
-//        });
-
-
-//        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClicked(int itemPosition, Object dataObject) {
-//                makeToast(getActivity(), "Clicked!");
-//            }
-//        });
-
     }
-
-    static void makeToast(Context ctx, String s) {
-        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
-    }
-
-//
-//    @OnClick(R.id.right)
-//    public void right() {
-//        /**
-//         * Trigger the right event manually.
-//         */
-//        flingContainer.getTopCardListener().selectRight();
-//    }
-//
-//    @OnClick(R.id.left)
-//    public void left() {
-//        flingContainer.getTopCardListener().selectLeft();
-//    }
-
-
 }
