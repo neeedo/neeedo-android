ef9930a04c1a3888da95f5ab03f2779772e3b179
stephan lindauer <stephan.lindauer@gmail.com>
2015-02-04 11:59:28 +0100

    tinder layout changes

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/ServerConstants.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/ServerConstants.java
@@ -4,5 +4,4 @@ public abstract class ServerConstants {
     public static final String LOCALHOST_ADDRESS_STUB = "http://10.0.2.2:9000/stub/";
     public static final String LOCALHOST_ADDRESS = "http://10.0.2.2:9000/";
     public static final String LIVE_SERVER_ADDRESS = "http://dry-depths-2035.herokuapp.com/stub/";
-
 }
