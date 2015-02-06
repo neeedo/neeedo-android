6266e66bcbf85e5998cc39bc9734fd75aef2d675
TIIUNDER <felixthethunder@arcor.de>
2015-02-06 01:56:28 +0100

    list of real demands

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/ListCardsFragment.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/ListCardsFragment.java
@@ -1,17 +1,21 @@
 package neeedo.imimaprx.htw.de.neeedo.fragments;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
-
+import com.squareup.otto.Subscribe;
 import java.util.ArrayList;
 import java.util.List;
-
 import neeedo.imimaprx.htw.de.neeedo.R;
+import neeedo.imimaprx.htw.de.neeedo.entities.Demand;
+import neeedo.imimaprx.htw.de.neeedo.events.ServerResponseEvent;
+import neeedo.imimaprx.htw.de.neeedo.models.DemandsModel;
+import neeedo.imimaprx.htw.de.neeedo.rest.HttpGetAsyncTask;
+import neeedo.imimaprx.htw.de.neeedo.rest.SuperHttpAsyncTask;
+
+import neeedo.imimaprx.htw.de.neeedo.entities.Demands;
 
 public class ListCardsFragment extends SuperFragment {
 
@@ -32,12 +36,26 @@ public class ListCardsFragment extends SuperFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        // TODO fill list with card objects
+        SuperHttpAsyncTask asyncTask;
 
-        // arraylist with dummy items
+        // execute async task @Subscribe
+        asyncTask = new HttpGetAsyncTask();
+        asyncTask.execute();
+    }
+
+    @Subscribe
+    public void fillList(ServerResponseEvent e) {
+        // get demands out of demands model singleton
+        Demands demands = DemandsModel.getInstance().getDemands();
+
+        // get array list of demands
+        ArrayList<Demand> demandList = demands.getDemands();
+
+        // list items
         List<String> list = new ArrayList<String>();
-        for(int i = 1; i <= 10; i++) {
-            list.add("Card " + i); // fill dummy items
+        for(int i = 0; i < demandList.size(); i++) {
+            // TODO nice output format
+            list.add(demandList.get(i).toString()); // fill with demands
         }
 
         // array adapter shows items in list view
@@ -46,6 +64,5 @@ public class ListCardsFragment extends SuperFragment {
         listView.setAdapter(adapter);
 
         // TODO set item listeners for single view
-
     }
 }
