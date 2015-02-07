19bdfe288b9d1f83f409beeb4b40360ae8083146
TIIUNDER <felixthethunder@arcor.de>
2015-02-07 21:10:57 +0100

    create demand object out of form

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/NewDemandCardFragment.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/NewDemandCardFragment.java
@@ -5,10 +5,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import neeedo.imimaprx.htw.de.neeedo.entities.Demand;
 
 import neeedo.imimaprx.htw.de.neeedo.R;
+import neeedo.imimaprx.htw.de.neeedo.entities.Location;
+import neeedo.imimaprx.htw.de.neeedo.entities.Price;
 
 public class NewDemandCardFragment extends SuperFragment {
+
+    // fields
+    private EditText etMustTags;
+    private EditText etShouldTags;
+    private EditText etLocationLat;
+    private EditText etLocationLon;
+    private EditText etDistance;
+    private EditText etPriceMin;
+    private EditText etPriceMax;
+    private Button btnSubmit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,50 @@ public class NewDemandCardFragment extends SuperFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.new_demand_card_form, container, false);
+
+        // initalize fields
+        etMustTags = (EditText) view.findViewById(R.id.etMustTags);
+        etShouldTags = (EditText) view.findViewById(R.id.etShouldTags);
+        etLocationLat = (EditText) view.findViewById(R.id.etLocationLat);
+        etLocationLon = (EditText) view.findViewById(R.id.etLocationLon);
+        etDistance = (EditText) view.findViewById(R.id.etDistance);
+        etPriceMin = (EditText) view.findViewById(R.id.etPriceMin);
+        etPriceMax = (EditText) view.findViewById(R.id.etPriceMax);
+        btnSubmit = (Button) view.findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get form values
+                String etMustTagsText = etMustTags.getText().toString();
+                String etShouldTagsText = etShouldTags.getText().toString();
+                String etLocationLatText = etLocationLat.getText().toString();
+                String etLocationLonText = etLocationLon.getText().toString();
+                String etDistanceText = etDistance.getText().toString();
+                String etPriceMinText = etPriceMin.getText().toString();
+                String etPriceMaxText = etPriceMax.getText().toString();
+
+                // convert fields
+                ArrayList<String> mustTags = new ArrayList<String>(Arrays.asList(etMustTagsText.split(",")));
+                ArrayList<String> shouldTags = new ArrayList<String>(Arrays.asList(etShouldTagsText.split(",")));
+                Location location = new Location(Long.parseLong(etLocationLatText), Long.parseLong(etLocationLonText)); // TODO use double instead of long
+                int distance = Integer.parseInt(etDistanceText);
+                Price price = new Price(Double.parseDouble(etPriceMinText), Double.parseDouble(etPriceMaxText));
+
+                // create new demand
+                Demand demand = new Demand();
+                demand.setMustTags(mustTags);
+                demand.setShouldTags(shouldTags);
+                demand.setLocation(location);
+                demand.setDistance(distance);
+                demand.setPrice(price);
+
+                System.out.println(demand);
+
+                // TODO send data
+            }
+        });
+
         return view;
     }
 }
