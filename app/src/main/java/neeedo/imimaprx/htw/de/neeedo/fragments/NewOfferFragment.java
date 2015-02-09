bdae473bb6e8f07ed289b1d2341b9ebf4680051e
TIIUNDER <felixthethunder@arcor.de>
2015-02-09 20:43:22 +0100

    new offer form edit for current offer API model

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/NewOfferFragment.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/NewOfferFragment.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import neeedo.imimaprx.htw.de.neeedo.R;
 
 public class NewOfferFragment extends SuperFragment {
+
+    // fields
+    private EditText etTags;
+    private EditText etLocationLat;
+    private EditText etLocationLon;
+    private EditText etPrice;
+    private Button btnSubmit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,17 @@ public class NewOfferFragment extends SuperFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.new_offer_form, container, false);
+
+        // initalize fields
+        etTags = (EditText) view.findViewById(R.id.etTags);
+        etLocationLat = (EditText) view.findViewById(R.id.etLocationLat);
+        etLocationLon = (EditText) view.findViewById(R.id.etLocationLon);
+        etPrice = (EditText) view.findViewById(R.id.etPrice);
+        btnSubmit = (Button) view.findViewById(R.id.btnSubmit);
+
+        // TODO create new offer
+        btnSubmit.setEnabled(false); // TODO enable button
+
         return view;
     }
 }
