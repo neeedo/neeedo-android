1cf1c748f8cd81d4a253b8d060b2398af3eb8e00
GammaG <m_seidler@hotmail.de>
2015-04-26 17:02:13 +0200

    Last Testcases for Entites added

--- app/src/test/java/neeedo/imimaprx/htw/de/neeedo/entities/SingleOfferTest.java
+++ app/src/test/java/neeedo/imimaprx/htw/de/neeedo/entities/SingleOfferTest.java
@@ -1,23 +1,68 @@
 package neeedo.imimaprx.htw.de.neeedo.entities;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
-public class SingleOfferTest {
+public class SingleOfferTest extends Assert {
+
+    private SingleOffer singleOffer;
+    private Offer offer;
+
+    @Before
+    public void setUp() {
+
+        offer = new Offer();
+        offer.setPrice(55d);
+        offer.setLocation(new Location(6, 6));
+        offer.setId("testlauf");
+        offer.setVersion(6);
+        offer.setUserId("some number");
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("zeugs");
+        list.add("other zeugs");
+
+        offer.setTags(list);
+
+        singleOffer = new SingleOffer();
+        singleOffer.setOffer(offer);
+
+    }
+
 
     @Test
     public void testGetOffer() throws Exception {
+
+        assertTrue(singleOffer.getOffer().getId().equals(offer.getId()));
+
 
     }
 
     @Test
     public void testSetOffer() throws Exception {
 
+        Offer localOffer = new Offer();
+        localOffer.setPrice(55d);
+        localOffer.setLocation(new Location(6, 6));
+        localOffer.setId("blakeks");
+        localOffer.setVersion(6);
+        localOffer.setUserId("some number");
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("zeugs");
+        list.add("other zeugs");
+
+        localOffer.setTags(list);
+
+        singleOffer.setOffer(localOffer);
+
+        assertFalse(singleOffer.getOffer().getId().equals(offer.getId()));
+
+
     }
 
-    @Test
-    public void testToString() throws Exception {
 
-    }
 }
