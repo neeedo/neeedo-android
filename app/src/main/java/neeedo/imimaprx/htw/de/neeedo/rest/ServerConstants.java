c87f0ee65d735fe289ba04c683892ce6d9cfda62
TIIUNDER <felixthethunder@arcor.de>
2015-05-05 10:51:15 +0200

    local https server address constant

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/ServerConstants.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/ServerConstants.java
@@ -3,10 +3,11 @@ package neeedo.imimaprx.htw.de.neeedo.rest;
 public abstract class ServerConstants {
     public static final String LOCALHOST_ADDRESS_STUB = "http://10.0.2.2:9000/stub/";
     public static final String LOCALHOST_ADDRESS = "http://10.0.2.2:9000/";
+    public static final String LOCALHOST_ADDRESS_SSL = "https://10.0.2.2:9443/";
     public static final String LIVE_SERVER_ADDRESS = "http://46.101.162.213/";
-    public static final String LIVE_SERVER_ADDRESS_SSL = "https://10.0.2.2:9443/";
+    public static final String LIVE_SERVER_ADDRESS_SSL = "http://46.101.162.213/";
     //public static final String ACTIVE_SERVER = LOCALHOST_ADDRESS;
     //public static final String ACTIVE_SERVER = LIVE_SERVER_ADDRESS;
-    public static final String ACTIVE_SERVER = LIVE_SERVER_ADDRESS_SSL;
+    public static final String ACTIVE_SERVER = LOCALHOST_ADDRESS_SSL;
 
 }
