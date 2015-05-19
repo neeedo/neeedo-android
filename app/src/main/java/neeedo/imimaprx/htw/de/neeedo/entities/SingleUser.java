5f4f48a7dd9aee36f65164014c199caab5da795f
GammaG <m_seidler@hotmail.de>
2015-05-19 11:16:09 +0200

    errorcode added

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/entities/SingleUser.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/entities/SingleUser.java
@@ -11,11 +11,26 @@ public class SingleUser implements Serializable {
     @Element
     private User user;
 
+    @Element
+    private String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
     public User getUser() {
         return user;
     }
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public boolean hasErrorCode() {
+        return !(error == null);
     }
 }
