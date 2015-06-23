64b6b1768641f2182b35bd3eb4f0277cfee0e058
GammaG <m_seidler@hotmail.de>
2015-06-23 13:15:25 +0200

    bugfix json send

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/matching/GetOffersToDemandAsyncTask.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/rest/matching/GetOffersToDemandAsyncTask.java
@@ -16,9 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import neeedo.imimaprx.htw.de.neeedo.entities.demand.Demand;
-import neeedo.imimaprx.htw.de.neeedo.entities.demand.DemandMin;
 import neeedo.imimaprx.htw.de.neeedo.entities.offer.Offers;
-import neeedo.imimaprx.htw.de.neeedo.entities.user.UserMin;
 import neeedo.imimaprx.htw.de.neeedo.events.FoundMatchesEvent;
 import neeedo.imimaprx.htw.de.neeedo.factory.HttpRequestFactoryProviderImpl;
 import neeedo.imimaprx.htw.de.neeedo.models.OffersModel;
@@ -46,15 +44,16 @@ public class GetOffersToDemandAsyncTask extends BaseAsyncTask {
     protected Object doInBackground(Object[] params) {
         try {
 
-            DemandMin demandMin = createDemandMinByDemand(demand);
             final String url = ServerConstantsUtils.getActiveServer() + "matching/demand";
             HttpHeaders requestHeaders = new HttpHeaders();
             setAuthorisationHeaders(requestHeaders);
 
+            requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
             List<MediaType> acceptableMediaTypes = new ArrayList<>();
             acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
             requestHeaders.setAccept(acceptableMediaTypes);
-            HttpEntity<DemandMin> requestEntity = new HttpEntity<DemandMin>(demandMin, requestHeaders);
+            HttpEntity<Demand> requestEntity = new HttpEntity<Demand>(demand, requestHeaders);
             RestTemplate restTemplate = new RestTemplate(HttpRequestFactoryProviderImpl.getClientHttpRequestFactorySSLSupport(5000));
             restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
             restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
@@ -66,23 +65,5 @@ public class GetOffersToDemandAsyncTask extends BaseAsyncTask {
             Log.e(this.getClass().getSimpleName(), e.getMessage(), e);
             return new RestResult(this.getClass().getSimpleName(), RestResult.ReturnType.FAILED);
         }
-    }
-
-
-    //Needed because Json ignore does not work somehow
-    private DemandMin createDemandMinByDemand(Demand demand) {
-
-        DemandMin demandMin = new DemandMin();
-        UserMin userMin = new UserMin(demand.getUser().getId(), demand.getUser().getEmail());
-        demandMin.setVersion(demand.getVersion());
-        demandMin.setId(demand.getId());
-        demandMin.setDistance(demand.getDistance());
-        demandMin.setLocation(demand.getLocation());
-        demandMin.setMustTags(demand.getMustTags());
-        demandMin.setShouldTags(demand.getShouldTags());
-        demandMin.setPrice(demand.getPrice());
-        demandMin.setUser(userMin);
-        return demandMin;
-
     }
 }
