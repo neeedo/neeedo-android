7ca227cfd3445a5f480a29dc2f451c6fa3ae7abf
TIIUNDER <felixthethunder@arcor.de>
2015-06-30 14:39:04 +0200

    added clickable example tags to demand form

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/FormDemandFragment.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/FormDemandFragment.java
@@ -6,7 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import neeedo.imimaprx.htw.de.neeedo.R;
 import neeedo.imimaprx.htw.de.neeedo.entities.util.Location;
@@ -21,6 +24,7 @@ public class FormDemandFragment extends FormFragment {
     protected final ActiveUser activeUser = ActiveUser.getInstance();
 
     protected MultiAutoCompleteTextView etMustTags;
+    protected LinearLayout llMustTags;
     protected MultiAutoCompleteTextView etShouldTags;
     protected EditText etLocationLat;
     protected EditText etLocationLon;
@@ -53,6 +57,7 @@ public class FormDemandFragment extends FormFragment {
         View view = inflater.inflate(R.layout.form_demand_view, container, false);
 
         etMustTags = (MultiAutoCompleteTextView) view.findViewById(R.id.etMustTags);
+        llMustTags = (LinearLayout) view.findViewById(R.id.llMustTags);
         etShouldTags = (MultiAutoCompleteTextView) view.findViewById(R.id.etShouldTags);
         etLocationLat = (EditText) view.findViewById(R.id.etLocationLat);
         etLocationLon = (EditText) view.findViewById(R.id.etLocationLon);
@@ -70,6 +75,22 @@ public class FormDemandFragment extends FormFragment {
 
         etMustTags.setOnClickListener(new AutocompletionOnClickListener(etMustTags));
         etShouldTags.setOnClickListener(new AutocompletionOnClickListener(etShouldTags));
+
+        // TODO use real suggestions for tags
+        for(int i = 0; i < 3; i++) {
+            final TextView tvMustTag = new TextView(getActivity());
+            tvMustTag.setText("tag"+i);
+            tvMustTag.setPadding(0, 0, 5, 0);
+            llMustTags.addView(tvMustTag);
+            tvMustTag.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String tag = tvMustTag.getText().toString();
+                    etMustTags.setText(etMustTags.getText().toString() + tag + ", ");
+                    etMustTags.setSelection(etMustTags.length());
+                }
+            });
+        }
 
         return view;
     }
