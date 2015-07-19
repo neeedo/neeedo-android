76733833c97d36a6ac586d39e7cf6d5d2fea6697
TIIUNDER <felixthethunder@arcor.de>
2015-07-19 23:50:42 +0200

    use products list for favorites

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/ListFavoritesFragment.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/ListFavoritesFragment.java
@@ -7,18 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.squareup.otto.Subscribe;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import neeedo.imimaprx.htw.de.neeedo.R;
 import neeedo.imimaprx.htw.de.neeedo.entities.favorites.Favorite;
-import neeedo.imimaprx.htw.de.neeedo.entities.favorites.Favorites;
 import neeedo.imimaprx.htw.de.neeedo.events.UserFavoritesLoadedEvent;
 import neeedo.imimaprx.htw.de.neeedo.fragments.adapters.ListProductsArrayAdapter;
 import neeedo.imimaprx.htw.de.neeedo.models.ActiveUser;
@@ -30,9 +27,6 @@ public class ListFavoritesFragment extends SuperFragment {
     private final ActiveUser activeUser = ActiveUser.getInstance();
     private ListView listView;
     private View view;
-    private EditText text;
-    private List<Favorite> favoriteList;
-    private ListProductsArrayAdapter<Favorite> favoritArrayAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,39 +37,9 @@ public class ListFavoritesFragment extends SuperFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        view = inflater.inflate(R.layout.list_favorites_view, container, false);
+        view = inflater.inflate(R.layout.list_products_view, container, false);
 
-        listView = (ListView) view.findViewById(R.id.favorites_view_listView);
-
-        text = (EditText) view.findViewById(R.id.favorites_view_edit_text);
-
-        favoriteList = new ArrayList<>();
-
-//        favoritArrayAdapter = new ListProductsArrayAdapter<>(getActivity(), R.layout.list_products_item, favoriteList);
-//        listView.setAdapter(favoritArrayAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Favorite favorite = (Favorite) listView.getItemAtPosition(position);
-
-                FragmentManager fragmentManager = getFragmentManager();
-                Fragment fragment = new SingleOfferFragment();
-
-                Bundle args = new Bundle();
-                args.putString("id", favorite.getId());
-                fragment.setArguments(args);
-
-                fragmentManager.beginTransaction()
-                        .addToBackStack(null)
-                        .replace(R.id.container, fragment)
-                        .commit();
-            }
-        });
-
-        if (activeUser.hasActiveUser()) {
-            new GetFavoritesByIDAsyncTask(activeUser.getUserId()).execute();
-        }
+        listView = (ListView) view.findViewById(R.id.listview);
 
         return view;
     }
@@ -85,25 +49,50 @@ public class ListFavoritesFragment extends SuperFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        if (activeUser.hasActiveUser()) {
+            new GetFavoritesByIDAsyncTask(activeUser.getUserId()).execute();
+        }
 
     }
 
     @Subscribe
     public void fillList(UserFavoritesLoadedEvent e) {
 
-        favoriteList = FavoritesModel.getInstance().getFavorites();
+        ArrayList<Favorite> favoriteList = FavoritesModel.getInstance().getFavorites();
+
+        TextView tvEmpty = (TextView) view.findViewById(R.id.tvEmpty);
 
         if (favoriteList.isEmpty()) {
-            listView.setVisibility(View.INVISIBLE);
-            text.setVisibility(View.VISIBLE);
+            listView.setVisibility(View.GONE);
+            tvEmpty.setText(getActivity().getString(R.string.favorites_view_no_favorites_found));
+            tvEmpty.setVisibility(View.VISIBLE);
             return;
         } else {
-            listView.setVisibility(View.VISIBLE);
-            text.setVisibility(View.INVISIBLE);
-        }
+            ListProductsArrayAdapter<Favorite> favoritArrayAdapter = new ListProductsArrayAdapter<>(getActivity(), R.layout.list_products_item, favoriteList);
+            listView.setAdapter(favoritArrayAdapter);
 
-        favoritArrayAdapter = new ListProductsArrayAdapter<>(getActivity(), R.layout.list_products_item, favoriteList);
-        listView.setAdapter(favoritArrayAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Favorite favorite = (Favorite) listView.getItemAtPosition(position);
+
+                    FragmentManager fragmentManager = getFragmentManager();
+                    Fragment fragment = new SingleOfferFragment();
+
+                    Bundle args = new Bundle();
+                    args.putString("id", favorite.getId());
+                    fragment.setArguments(args);
+
+                    fragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.container, fragment)
+                            .commit();
+                }
+            });
+
+            listView.setVisibility(View.VISIBLE);
+            tvEmpty.setVisibility(View.GONE);
+        }
 
     }
 }
