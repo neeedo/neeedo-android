7a11a3b734e79580f4eb9b09e012bc00e639f3b8
TIIUNDER <felixthethunder@arcor.de>
2015-07-20 21:19:49 +0200

    removed ActionBar, added loading animation to image activity

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/ImageActivity.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/ImageActivity.java
@@ -1,25 +1,29 @@
 package neeedo.imimaprx.htw.de.neeedo;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import neeedo.imimaprx.htw.de.neeedo.utils.ServerConstantsUtils;
 
-public class ImageActivity extends ActionBarActivity {
+public class ImageActivity extends Activity {
     public final static String IMAGES_LIST_EXTRA = "images-list-intent-data";
     public final static String IMAGES_LIST_POSITION_EXTRA = "images-list-position-intent-data";
     private View view;
     private Intent intent;
     private ImageView imageView;
+    private ProgressBar progressBar;
     private Button btnNext;
     private Button btnPrev;
     private ArrayList<String> imageList;
@@ -43,15 +47,29 @@ public class ImageActivity extends ActionBarActivity {
 
     private void loadImageView() {
         imageView = (ImageView) view.findViewById(R.id.imageView);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         btnNext = (Button) view.findViewById(R.id.btnNext);
         btnPrev = (Button) view.findViewById(R.id.btnPrev);
+
+        progressBar.setVisibility(View.VISIBLE);
 
         if(imageList != null) {
             String imageUrl = ServerConstantsUtils.getActiveServer() + "images/" + imageList.get(position);
 
             Log.d("Image", imageUrl);
 
-            Picasso.with(this).load(imageUrl).into(imageView);
+            Picasso.with(this).load(imageUrl).into(imageView, new Callback() {
+                @Override
+                public void onSuccess() {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onError() {
+                    progressBar.setVisibility(View.GONE);
+                    imageView.setImageDrawable(Resources.getSystem().getDrawable(R.drawable.no_image));
+                }
+            });
 
             if (position == 0) {
                 btnPrev.setVisibility(View.INVISIBLE);
