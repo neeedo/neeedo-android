860c3856f5855dd703925e0aa54a1ff524d7ebb7
stephan lindauer <stephan.lindauer@gmail.com>
2015-07-20 22:37:10 +0200

    highlighting current selection in drawer

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/NavigationDrawerFragment.java
@@ -2,10 +2,14 @@ package neeedo.imimaprx.htw.de.neeedo.fragments;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -14,6 +18,9 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
 
 import neeedo.imimaprx.htw.de.neeedo.R;
 
@@ -21,6 +28,11 @@ public class NavigationDrawerFragment extends SuperFragment {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+
+    private TextView newOfferBtn;
+    private TextView listOffersBtn;
+    private TextView newDemandBtn;
+    private TextView listDemandsBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,8 +78,16 @@ public class NavigationDrawerFragment extends SuperFragment {
                     inputManager.hideSoftInputFromWindow(drawerView.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                 }
             }
-        };
 
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                super.onDrawerStateChanged(newState);
+                if (newState == DrawerLayout.STATE_IDLE)
+                    highlightCurrentFragment();
+            }
+
+            
+        };
 
         drawerLayout.post(new Runnable() {
             @Override
@@ -84,7 +104,7 @@ public class NavigationDrawerFragment extends SuperFragment {
                              Bundle savedInstanceState) {
         LinearLayout navigationDrawerLayout = (LinearLayout) inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
 
-        View newOfferBtn = navigationDrawerLayout.findViewById(R.id.create_new_offer);
+        newOfferBtn = (TextView) navigationDrawerLayout.findViewById(R.id.create_new_offer);
         newOfferBtn.setOnClickListener(new OnClickListener() {
                                            @Override
                                            public void onClick(View v) {
@@ -93,7 +113,7 @@ public class NavigationDrawerFragment extends SuperFragment {
                                        }
         );
 
-        View listOffersBtn = navigationDrawerLayout.findViewById(R.id.list_offers);
+        listOffersBtn = (TextView) navigationDrawerLayout.findViewById(R.id.list_offers);
         listOffersBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,7 +121,7 @@ public class NavigationDrawerFragment extends SuperFragment {
             }
         });
 
-        View newDemandBtn = navigationDrawerLayout.findViewById(R.id.create_new_demand);
+        newDemandBtn = (TextView) navigationDrawerLayout.findViewById(R.id.create_new_demand);
         newDemandBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,7 +129,7 @@ public class NavigationDrawerFragment extends SuperFragment {
             }
         });
 
-        final View listDemandsBtn = navigationDrawerLayout.findViewById(R.id.list_demands);
+        listDemandsBtn = (TextView) navigationDrawerLayout.findViewById(R.id.list_demands);
         listDemandsBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -142,5 +162,47 @@ public class NavigationDrawerFragment extends SuperFragment {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void highlightCurrentFragment() {
+        List<Fragment> fragments = getActivity().getSupportFragmentManager().getFragments();
+
+        Fragment currentlyDisplayesFragment = null;
+
+        for (Fragment fragment : fragments) {
+            if (fragment.isVisible() && !(fragment instanceof NavigationDrawerFragment)) {
+                System.out.println(fragment.getClass().getName());
+                currentlyDisplayesFragment = fragment;
+                break;
+            }
+        }
+
+        if (currentlyDisplayesFragment == null)
+            return;
+
+        int inactiveBackgroundColor = 0xcc111111;
+        int inactiveTextColor = 0xffdddddd;
+
+        setColorsToTextField(newOfferBtn, inactiveBackgroundColor, inactiveTextColor);
+        setColorsToTextField(listOffersBtn, inactiveBackgroundColor, inactiveTextColor);
+        setColorsToTextField(newDemandBtn, inactiveBackgroundColor, inactiveTextColor);
+        setColorsToTextField(listDemandsBtn, inactiveBackgroundColor, inactiveTextColor);
+
+        int activeBackgroundColor = inactiveTextColor;
+        int activeTextColor = inactiveBackgroundColor;
+
+        if (currentlyDisplayesFragment instanceof NewOfferFragment)
+            setColorsToTextField(newOfferBtn, activeBackgroundColor, activeTextColor);
+        if (currentlyDisplayesFragment instanceof ListOffersFragment)
+            setColorsToTextField(listOffersBtn, activeBackgroundColor, activeTextColor);
+        if (currentlyDisplayesFragment instanceof NewDemandFragment)
+            setColorsToTextField(newDemandBtn, activeBackgroundColor, activeTextColor);
+        if (currentlyDisplayesFragment instanceof ListDemandsFragment)
+            setColorsToTextField(listDemandsBtn, activeBackgroundColor, activeTextColor);
+    }
+
+    private void setColorsToTextField(TextView textView, int backgroundColor, int textColor) {
+        textView.setBackgroundColor(backgroundColor);
+        textView.setTextColor(textColor);
     }
 }
