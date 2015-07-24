747595855b101ca9bd8e42fa72e77e7b5649a9fe
TIIUNDER <felixthethunder@arcor.de>
2015-07-24 22:41:56 +0200

    message user avatar

--- app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/adapters/MessageUserArrayAdapter.java
+++ app/src/main/java/neeedo/imimaprx/htw/de/neeedo/fragments/adapters/MessageUserArrayAdapter.java
@@ -1,6 +1,7 @@
 package neeedo.imimaprx.htw.de.neeedo.fragments.adapters;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ public class MessageUserArrayAdapter<Object> extends ArrayAdapter<Object> {
     private int layoutResourceId;
     private List<Object> users;
 
+    private TextView tvAvatar;
     private TextView tvNew;
     private TextView tvUser;
 
@@ -32,20 +34,25 @@ public class MessageUserArrayAdapter<Object> extends ArrayAdapter<Object> {
     public View getView(int position, View row, ViewGroup parent) {
         row = LayoutInflater.from(context).inflate(layoutResourceId, parent, false);
 
+        tvAvatar = (TextView) row.findViewById(R.id.tvAvatar);
         tvNew = (TextView) row.findViewById(R.id.tvNew);
         tvUser = (TextView) row.findViewById(R.id.tvUser);
 
         User user = (User) users.get(position);
 
         String userName = user.getName();
+        String userLetter = String.valueOf(userName.charAt(0)).toUpperCase();
         boolean hasNewMessages = user.isHasNewMessages();
 
         if(hasNewMessages) {
             tvNew.setVisibility(View.VISIBLE);
+            tvUser.setTypeface(null, Typeface.BOLD);
         } else {
             tvNew.setVisibility(View.GONE);
+            tvUser.setTypeface(null, Typeface.NORMAL);
         }
 
+        tvAvatar.setText(userLetter);
         tvUser.setText(userName);
 
         return row;
